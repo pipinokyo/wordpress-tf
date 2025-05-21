853f23c5fb20d9f4efd518e5f1c7f3982e00eabb
pipinokyo <cuneyt.omer@gmail.com>
2025-05-20 23:05:36 -0400

    updates

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-session-backend-bucket-cuneyt"
-    key     = "terraform.tfstate"
+    key     = "wordpress-tf/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
